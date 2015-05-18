6201610132cec165b087d9ef973f79849b6e565e
Bryce Langlotz <BryceLanglotzCS@gmail.com>
2015-05-18 14:30:38 -0400

    Facebook SDK Added

--- app/src/main/java/speakeasy/brycelanglotz/com/speakeasy/MainActivity.java
+++ app/src/main/java/speakeasy/brycelanglotz/com/speakeasy/MainActivity.java
@@ -18,6 +18,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
@@ -45,6 +46,8 @@ public class MainActivity extends ActionBarActivity
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
     }
 
     @Override
