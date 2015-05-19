8169eee5b829060ad5216622a88fea758860c090
wroyston <wesroyston@gmail.com>
2015-05-19 18:39:39 -0400

    MyAccount Class Added

--- app/src/main/java/speakeasy/brycelanglotz/com/speakeasy/MainActivity.java
+++ app/src/main/java/speakeasy/brycelanglotz/com/speakeasy/MainActivity.java
@@ -92,7 +92,7 @@ public class MainActivity extends ActionBarActivity
                 break;
             case 2:
                 fragmentManager.beginTransaction()
-                        .replace(R.id.container, MyAccount.newInstance(position + 1))
+                        .replace(R.id.container, MyAccountFragment.newInstance(position + 1))
                         .commit();
                 break;
             case 3:
@@ -241,6 +241,51 @@ public class MainActivity extends ActionBarActivity
                 System.out.println(mCostEditText.getText());
             }
         };
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
+    public static class MyAccountFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        private Button mGenerateQRCodeButton;
+        private EditText mCostEditText;
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static MyAccountFragment newInstance(int sectionNumber) {
+            MyAccountFragment fragment = new MyAccountFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public MyAccountFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my_account, container, false);
+            return rootView;
+        }
 
         @Override
         public void onActivityResult(int requestCode, int resultCode, Intent data) {
