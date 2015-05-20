2d93a37562e445307fcf76a67d3413c597b0824b
wroyston <wesroyston@gmail.com>
2015-05-20 17:50:47 -0400

    Add Meals Class

--- app/src/main/java/speakeasy/brycelanglotz/com/model/Meals.java
+++ app/src/main/java/speakeasy/brycelanglotz/com/model/Meals.java
@@ -10,4 +10,53 @@ public class Meals extends ParseObject {
     public Meals() {
         // A default constructor is required.
     }
+
+    public Meals(Boolean claimed, String claimedBy, Integer potentialRewards,
+                 String restaurantObjectId, String serverObjectId) {
+        setClaimed(claimed);
+        setClaimedBy(claimedBy);
+        setPotentialReward(potentialRewards);
+        setRestaurantObjectId(restaurantObjectId);
+        setServerObjectId(serverObjectId);
+    }
+
+    public Boolean getClaimed() {
+        return (Boolean) this.get("claimed");
+    }
+
+    public void setClaimed(Boolean claimed) {
+        this.put("claimed", claimed);
+    }
+
+    public String getClaimedBy() {
+        return (String) this.get("claimed_by");
+    }
+
+    public void setClaimedBy(String claimedBy) {
+        this.put("claimed_by", claimedBy);
+    }
+
+    public Integer getPotentialReward() {
+        return (Integer) this.get("potential_reward");
+    }
+
+    public void setPotentialReward(Integer potentialReward) {
+        this.put("potential_reward", potentialReward);
+    }
+
+    public String getRestaurantObjectId() {
+        return (String) this.get("restaurant_objectId");
+    }
+
+    public void setRestaurantObjectId(String restaurant) {
+        this.put("restaurant_objectId", restaurant);
+    }
+
+    public String getServerObjectId() {
+        return (String) this.get("server");
+    }
+
+    public void setServerObjectId(String server) {
+        this.put("server_objectId", server);
+    }
 }
