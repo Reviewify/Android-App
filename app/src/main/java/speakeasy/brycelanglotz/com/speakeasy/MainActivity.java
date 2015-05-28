96cd717b30a9de5136e142d0e2ce136f5a066977
Bryce Langlotz <BryceLanglotzCS@gmail.com>
2015-05-27 20:56:25 -0400

    Working Zing Intent scan

--- app/src/main/java/speakeasy/brycelanglotz/com/speakeasy/MainActivity.java
+++ app/src/main/java/speakeasy/brycelanglotz/com/speakeasy/MainActivity.java
@@ -24,24 +24,30 @@ import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 
+import com.google.zxing.integration.android.IntentResult;
 import com.parse.FindCallback;
 
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import com.parse.FunctionCallback;
 import com.parse.GetCallback;
+import com.parse.ParseCloud;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import speakeasy.brycelanglotz.com.model.Meals;
 import speakeasy.brycelanglotz.com.model.Reviews;
@@ -96,8 +102,9 @@ public class MainActivity extends ActionBarActivity
         switch (position) {
             case 0:
                 fragmentManager.beginTransaction()
-                        .replace(R.id.container, NativeCameraFragment.newInstance(position + 1))
+                        .replace(R.id.container, ReviewFragment.newInstance(position + 1))
                         .commit();
+                IntentIntegrator.initiateScan(this, "QR_CODE", "Scan QR Code Here");
                 break;
             case 1:
                 fragmentManager.beginTransaction()
@@ -113,6 +120,33 @@ public class MainActivity extends ActionBarActivity
                 ParseUser.logOut();
                 presentLoginActivity();
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+            String results = scanResult.getContents();
+            String[] components = results.split(" ");
+            HashMap<String, String> parameters = new HashMap<String, String>();
+            parameters.put("restaurantCode", components[0]);
+            parameters.put("mealCode", components[1]);
+            ParseCloud.callFunctionInBackground("VerifyMeal", parameters, new FunctionCallback<Object>() {
+                public void done(Object object, ParseException e) {
+                    if (e == null) {
+                        Meals meal = (Meals) object;
+                        Toast.makeText(getApplicationContext(), meal.getClaimedString(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), e.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
         }
     }
 
@@ -164,6 +198,90 @@ public class MainActivity extends ActionBarActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class ReviewFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        private Button mGenerateQRCodeButton;
+        private EditText mCostEditText;
+        private ImageView mQRCodeImageView;
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static ReviewFragment newInstance(int sectionNumber) {
+            ReviewFragment fragment = new ReviewFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public ReviewFragment() {
+        }
+
+
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_review, container, false);
+            // mGenerateQRCodeButton = (Button) rootView.findViewById(R.id.generateQRCodeButton);
+            // mGenerateQRCodeButton.setOnClickListener(generateQRCodeOnClickListener);
+            return rootView;
+        }
+
+        View.OnClickListener generateQRCodeOnClickListener = new View.OnClickListener() {
+            public void onClick(View v) {
+                setQRCodeWithString(ParseUser.getCurrentUser().getUsername() + mCostEditText.getText());
+            }
+        };
+
+        private void setQRCodeWithString(String data) {
+            try {
+                QRCodeWriter writer = new QRCodeWriter();
+                BitMatrix matrix = writer.encode(
+                        data, BarcodeFormat.QR_CODE, 400, 400
+                );
+                Bitmap bmp = toBitmap(matrix);
+                mQRCodeImageView.setImageBitmap(bmp);
+            } catch (WriterException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private static Bitmap toBitmap(BitMatrix matrix){
+            int height = matrix.getHeight();
+            int width = matrix.getWidth();
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+            for (int x = 0; x < width; x++){
+                for (int y = 0; y < height; y++){
+                    bmp.setPixel(x, y, matrix.get(x,y) ? Color.BLACK : Color.WHITE);
+                }
+            }
+            return bmp;
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
+        }
     }
 
     /**
